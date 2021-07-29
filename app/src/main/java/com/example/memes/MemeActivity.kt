ded2f7d6d08f1b162be1d1e0545690faaf47e16c
Tanya <13tanyash@gmail.com>
2021-07-29 22:45:46 +0530

    added ui, drawables, mvvm setup

--- app/src/main/java/com/example/memes/MemeActivity.kt
+++ app/src/main/java/com/example/memes/MemeActivity.kt
@@ -3,9 +3,16 @@ package com.example.memes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MemeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViewModel()
     }
+
+    private fun setupViewModel() {
+
+    }
+
 }
